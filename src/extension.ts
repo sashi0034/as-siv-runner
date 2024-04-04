09befce306f6260935fcb87d7f9f783636b6f70c
sashi0034 <satoshi3104.nishi@gmail.com>
2024-04-04 20:38:09 +0900

    実行機能の準備

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,49 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from "node:path";
+import * as child_process from "child_process";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "as-siv-runner" is now active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "as-siv-runner" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('as-siv-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from as-siv-runner!');
-	});
+    context.subscriptions.push(vscode.commands.registerCommand('as-siv-runner.runMain', () => {
+        runSiv3D(context, 'Main');
+    }));
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push((vscode.commands.registerCommand('as-siv-runner.runTest', () => {
+        runSiv3D(context, 'Test');
+    })));
 }
 
+function runSiv3D(context: vscode.ExtensionContext, entryPoint: string) {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        const document = editor.document;
+        const filePath = document.uri.fsPath;
+        vscode.window.showInformationMessage(`Run '${entryPoint} in '${filePath}'`);
+    } else {
+        vscode.window.showErrorMessage('No file selected for execution.');
+    }
+
+    // 拡張機能のディレクトリ内の実行ファイルを実行
+    console.log(context.extensionPath);
+    // const execPath = path.join(context.extensionPath, 'path/to/your/executable');
+    // child_process.execFile(execPath, (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.error(`実行エラー: ${error}`);
+    //         return;
+    //     }
+    //     console.log(`標準出力: ${stdout}`);
+    // });
+}
+
+
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
